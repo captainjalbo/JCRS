a2e13831159a4ee0f91639f15450143d34ae3705
captainjalbo <143764717+captainjalbo@users.noreply.github.com>
2024-10-24 20:13:20 -0400

    Update Logistics_Contract.sol
    Version of the JCRS smartcontract that is compatible with fleetbase

--- Logistics_Contract.sol
+++ Logistics_Contract.sol
@@ -1,29 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IEAS {
-    function getAttestation(address issuer, address subject, bytes32 schema) external view returns (bool);
-}
-
 contract LogisticsContract {
     address public sender;
     address public courier;
     address public receiver;
     uint public contractValue;
-    bool public packageApproved = false;
-    IEAS public eas;
-    bytes32 public schemaId;
-
-    enum ContractState { Created, TrustSetup, KeyShared, DepositMade, PackageSent, Approved, Shipping, Completed, Cancelled }
-    ContractState public state;
+    string public fleetBaseOrderId;
+    string public currentStatus;
 
     modifier onlySender() {
         require(msg.sender == sender, "Not authorized: Only sender can call this");
-        _;
-    }
-
-    modifier onlyCourier() {
-        require(msg.sender == courier, "Not authorized: Only courier can call this");
         _;
     }
 
@@ -31,68 +18,18 @@ contract LogisticsContract {
         address _courier, 
         address _receiver, 
         uint _contractValue, 
-        address _easAddress,
-        bytes32 _schemaId
+        string memory _fleetBaseOrderId
     ) {
         sender = msg.sender;
         courier = _courier;
         receiver = _receiver;
         contractValue = _contractValue;
-        eas = IEAS(_easAddress);
-        schemaId = _schemaId;
-        state = ContractState.Created;
+        fleetBaseOrderId = _fleetBaseOrderId;
     }
 
-    function TrustSetup() public onlySender {
-        require(state == ContractState.Created, "Invalid state");
-        state = ContractState.TrustSetup;
-    }
-
-    function ShareKey() public onlySender {
-        require(state == ContractState.TrustSetup, "Invalid state");
-        state = ContractState.KeyShared;
-    }
-
-    function DepositMoney() public payable onlySender {
-        require(state == ContractState.KeyShared, "Invalid state");
-        require(msg.value == contractValue, "Incorrect deposit amount");
-        state = ContractState.DepositMade;
-    }
-
-    function PackageSent() public onlyCourier {
-        require(state == ContractState.DepositMade, "Invalid state");
-        state = ContractState.PackageSent;
-    }
-
-    function PackageApproved() public onlyCourier {
-        require(state == ContractState.PackageSent, "Invalid state");
-        require(
-            eas.getAttestation(courier, address(this), schemaId),
-            "EAS attestation required for package approval"
-        );
-        packageApproved = true;
-        state = ContractState.Approved;
-    }
-
-    function IparcelShipping() public onlyCourier {
-        require(packageApproved, "Package not approved");
-        require(state == ContractState.Approved, "Invalid state");
-        require(
-            eas.getAttestation(courier, address(this), schemaId),
-            "EAS attestation required for iParcel shipping"
-        );
-        state = ContractState.Shipping;
-    }
-
-    function completeTransaction() public onlySender {
-        require(state == ContractState.Shipping, "Package not in shipping state");
-        state = ContractState.Completed;
-        payable(courier).transfer(address(this).balance);
-    }
-
-    function cancelContract() public onlySender {
-        require(state != ContractState.Completed, "Cannot cancel a completed contract");
-        state = ContractState.Cancelled;
-        payable(sender).transfer(address(this).balance);
+    function updateStatusFromFleetBase(string memory newStatus) public onlySender {
+        // Update the status received from FleetBase
+        currentStatus = newStatus;
+        // Additional logic based on the status, such as triggering attestations
     }
 }
